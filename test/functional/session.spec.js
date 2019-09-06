729814dc5ee977d2a720b32528fe143b50730f46
Fabio <fabio.junior@uotz.com.br>
2019-09-06 15:37:05 -0300

    Implementação de factory para criar usuários para testes funcionais

--- test/functional/session.spec.js
+++ test/functional/session.spec.js
@@ -1,26 +1,28 @@
 'use strict';
 
 const { test, trait } = use('Test/Suite')('Session');
-trait('Test/ApiClient');
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory');
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const User = use('App/Models/User');
+// const User = use('App/Models/User');
+
+trait('Test/ApiClient');
 
 test('it should return JWT token when session created', async ({ assert, client }) => {
-  const user = await User.create({
-    name: 'Fabio Jr',
+  const sessionPayload = {
     email: 'fabio.junior@uotz.com.br',
     password: '010203'
-  });
+  };
+
+  const user = await Factory.model('App/Models/User').create(sessionPayload);
+  console.log(user);
 
   const response = await client
     .post('/sessions')
-    .send({
-      email: 'fabio.junior@uotz.com.br',
-      password: '010203'
-    })
+    .send(sessionPayload)
     .end();
-  console.log(response.body.token);
   response.assertStatus(200);
   assert.exists(response.body.token);
 });
